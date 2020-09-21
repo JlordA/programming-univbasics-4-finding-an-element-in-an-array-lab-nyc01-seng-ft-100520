f913f651989ce39dff2e6724c505fd0d77be354a
placid-tier-4550 <james.ardery@gmail.com>
2020-09-21 21:58:56 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,10 @@
 def find_element_index(array, value_to_find)
   counter = 0
+  found_value_index = nil
   while counter < array.length do
-    puts array[counter] == value_to_find
+    if array[counter] == value_to_find
+      return counter
+    end
     counter += 1
   end
 end
